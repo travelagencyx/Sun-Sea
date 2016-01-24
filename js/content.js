811acece0111372e5a21feb1fd20f4a0a3ede012
travelagencyx <travelagencyx@gmail.com>
2016-01-24 11:25:45 -0500

    banner and teaser

--- js/content.js
+++ js/content.js
@@ -1,1 +1,50 @@
-document.write('<div class="slider"></div><div class="banner"></div><div class="teaser"></div><div class="about"></div>');
+document.write(`
+	<div class="content">
+		<div class="slider">
+		</div>
+		<div class="banner">
+			<div class="bannerBox">
+				<span class="bannerBoxTitle">
+				Vaš specijalist za odmor od 2001
+				</span>
+				<span class="bannerBoxText">
+				Više od 200 000 zadovoljnih gostiju
+				</span>
+			</div>
+			<div class="bannerBox">
+				<span class="bannerBoxTitle">
+				Garancija kvalitete
+				</span>
+				<span class="bannerBoxText">
+				Radimo direktno sa iznajmljivačima
+				</span>
+			</div>
+			<div class="bannerBox">
+				<span class="bannerBoxTitle">
+				Poslovnice na Jadranskoj obali
+				</span>
+				<span class="bannerBoxText">
+				Split, Rogoznica, Vodice, Budva
+				</span>
+			</div>
+		</div>
+		<div class="teaser">
+			<div class="teaserBig">
+			</div>
+			<div class="teaserSmall">
+				<div class="teaserSmallTopFirst">
+				</div>
+				<div class="teaserSmallBottomFirst">
+				</div>
+			</div>
+			<div class="teaserSmall">
+				<div class="teaserSmallTopSecond">
+				</div>
+				<div class="teaserSmallBottomSecond">
+				</div>
+			</div>
+		</div>
+		<div class="about">
+		</div>
+	</div>
+`);
