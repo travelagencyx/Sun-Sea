a31b0145a9bf900f0a65ef8d53094de7c57e44ab
bumblebee24 <martina.prgomet@gmail.com>
2016-01-30 16:59:33 +0100

    Header

--- js/header.js
+++ js/header.js
@@ -1,7 +1,50 @@
-document.write('<div class="head">' +
-  '<div class="wrapper">' +
-    '<div class="row">' +
-      'header' +
-    '</div>' +
-  '</div>' +
-'</div>');
+document.write(`<div class="head">
+  <div class="logo-wrapper">
+    <div>
+      <div class="header-left">
+        <a href="#">
+          <img id="logo" alt="Sun and Sea" src="images/logo.png">
+        </a>
+      </div>
+      <div class="header-right">
+        <ul class="list">
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 2</a>
+          </li>
+          <li>
+            <a href="#">Link 3</a>
+          </li>
+          <li>
+            <a href="#">Link 4</a>
+          </li>
+          <li>
+            <a href="#">Link 5</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="nav">
+      <ul class="menu clearfix menu-images">
+        <li class="menu-item">
+          <a href="#">Accomodation</a>
+        </li>
+        <li class="menu-item">
+          <a href="#">Destinations</a>
+        </li>
+        <li class="menu-item">
+          <a href="#">Cruises</a>
+        </li>
+        <li class="menu-item">
+          <a href="#">Rent a car</a>
+        </li>
+        <li class="menu-item">
+          <a href="#">Travel services</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+`);
