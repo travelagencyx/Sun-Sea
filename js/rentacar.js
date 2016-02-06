ee63b5a11254956c3f663ab0b7e5fd01b0c41dd2
bumblebee24 <martina.prgomet@gmail.com>
2016-02-06 14:20:51 +0100

    Rentacar page setup

--- js/rentacar.js
+++ js/rentacar.js
@@ -1,1 +1,17 @@
-document.write(`<div>rent a car</div>`)
+document.write(`<div class="content">
+  <div class="container">
+    <div class="contentInside">
+      <div class="breadcrumb marginBottomSmall paddingLeftSmall">
+        <span class="first-breadcrumb">
+          <a href="/"><span>Home</span></a>
+        </span>
+        <span class="last-breadcrumb">
+          <span>Rent a Car</span>
+        </span>
+      </div>
+    </div>
+    <div class="contentInside paddingBottomSmall">
+      <h1>Rent a Car</h1>
+    </div>
+  </div>
+</div>`)
