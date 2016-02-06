6161d4973122703ce48e070ad119532d2bac6643
bumblebee24 <martina.prgomet@gmail.com>
2016-02-06 15:07:06 +0100

    rentacar

--- js/rentacar.js
+++ js/rentacar.js
@@ -6,12 +6,29 @@ document.write(`<div class="content">
           <a href="/"><span>Home</span></a>
         </span>
         <span class="last-breadcrumb">
-          <span>Rent a Car</span>
+          <span>Rent a car</span>
         </span>
       </div>
     </div>
     <div class="contentInside paddingBottomSmall">
-      <h1>Rent a Car</h1>
+      <h1>Rent a car</h1>
+      <div class="marginsInside displayBlock">
+        <div class="rentacarimage"></div>
+        <div>
+          More then 10 years, fleet of more than 1000 cars, more than 100 000
+          satisfied clients. <br/><br/>
+          Choose the best rent a car in Croatia,
+          <b>LAST MINUTE Rent a car</b>.
+          Many years of experience, constant renewal of the car fleet, <br/>
+          investing in employee education, affordable prices and special
+          services for our customers are some of the reasons why we got <br/>
+          one of the best awards, <b>TUI</b> recognition for the lowest number
+          of complaints. <br/><br/>
+          Besides the satisfaction of our valued clients, we are also proud that
+          our work and labor are appreciated in Croatia and also on the global
+          market.<br/><br/>
+        </div>
+      </div>
     </div>
   </div>
 </div>`)
