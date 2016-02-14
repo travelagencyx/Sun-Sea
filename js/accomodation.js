ce6ceefe1febd24a1cee2c55b983d5d800df8524
travelagencyx <travelagencyx@gmail.com>
2016-02-14 11:15:00 -0500

    Accomodation

--- js/accomodation.js
+++ js/accomodation.js
@@ -13,27 +13,54 @@ document.write(`<div class="content">
     <div class="contentInside paddingBottomSmall">
       <h1>Accommodation</h1>
       <div class="displayBlock">
-		<div class="accommodationimage"></div>
+		<div class="accommodationimage">
+    </div>
 		<div class="banner">
-            <div>
-                <span class="textBoxMiddle">
+      <div>
+        <span class="textBoxMiddle">
 					Stay in one of our apartements located in heart of Split - Diocletian Palace.
-                </span>
-            </div>
-        </div>
-        <div class="width33">
-			
-        </div>
-		<div class="width33">
-          
-        </div>
-		<div class="width33">
-          
-        </div>
-        <div class="sliderCars">
-          CAROUSEL
-        </div>
+        </span>
       </div>
     </div>
+    <div class="width33">
+			<div class="galleryaccommodation">
+      </div>
+      <div class="accommodationinfo">
+        <div class="accommodationheadline">Apartment Skalice</div>
+        <hr>
+        <span class="threestars"></span>
+        <div class="persons">2-4 persons</div>
+        <div class="centerdistance">500m</div>
+        <div class="seadistance">1000m</div>
+      </div>
+    </div>
+		<div class="width33">
+      <div class="galleryaccommodation">
+      </div>
+      <div class="accommodationinfo">
+        <div class="accommodationheadline">Apartment Skalice</div>
+        <hr>
+        <span class="threestars"></span>
+        <div class="persons">2-4 persons</div>
+        <div class="centerdistance">500m</div>
+        <div class="seadistance">1000m</div>
+      </div>
+    </div>
+		<div class="width33">
+      <div class="galleryaccommodation">
+      </div>
+      <div class="accommodationinfo">
+        <div class="accommodationheadline">Apartment Skalice</div>
+        <hr>
+        <span class="threestars"></span>
+        <div class="persons">2-4 persons</div>
+        <div class="centerdistance">500m</div>
+        <div class="seadistance">1000m</div>
+      </div>
+    </div>
+    <div class="sliderCars">
+      CAROUSEL
+    </div>
   </div>
+</div>
 </div>`)
