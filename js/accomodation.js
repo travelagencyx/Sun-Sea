10ddb74d20e6ceac00bd61be5409cce60c73784c
travelagencyx <travelagencyx@gmail.com>
2016-02-14 16:04:07 -0500

    Apartment carousel

--- js/accomodation.js
+++ js/accomodation.js
@@ -1,4 +1,5 @@
-document.write(`<div class="content">
+document.write(`
+<div class="content">
   <div class="container">
     <div class="contentInside">
       <div class="breadcrumb marginBottomSmall paddingLeftSmall">
@@ -13,54 +14,51 @@ document.write(`<div class="content">
     <div class="contentInside paddingBottomSmall">
       <h1>Accommodation</h1>
       <div class="displayBlock">
-		<div class="accommodationimage">
-    </div>
-		<div class="banner">
-      <div>
-        <span class="textBoxMiddle">
-					Stay in one of our apartements located in heart of Split - Diocletian Palace.
-        </span>
+        <div class="accommodationcarousel"></div>
+    		<div class="banner">
+          <div>
+            <span class="textBoxMiddle">
+    					Stay in one of our apartements located in heart of Split - Diocletian Palace.
+            </span>
+          </div>
+        </div>
+        <div class="width33">
+    			<div class="galleryaccommodation">
+          </div>
+          <div class="accommodationinfo">
+            <div class="accommodationheadline">Apartment Skalice</div><div class="price">100 €<span class="pernight"> / per night</span></div>
+            <hr class="clear">
+            <span class="threestars"></span>
+            <div class="persons">2-4 persons</div>
+            <div class="centerdistance">500m</div>
+            <div class="seadistance">1000m</div>
+          </div>
+        </div>
+    		<div class="width33">
+          <div class="galleryaccommodation">
+          </div>
+          <div class="accommodationinfo">
+            <div class="accommodationheadline">Apartment Skalice</div><div class="price">100 €<span class="pernight"> / per night</span></div>
+            <hr class="clear">
+            <span class="threestars"></span>
+            <div class="persons">2-4 persons</div>
+            <div class="centerdistance">500m</div>
+            <div class="seadistance">1000m</div>
+          </div>
+        </div>
+    		<div class="width33">
+          <div class="galleryaccommodation">
+          </div>
+          <div class="accommodationinfo">
+            <div class="accommodationheadline">Apartment Skalice</div><div class="price">100 €<span class="pernight"> / per night</span></div>
+            <hr class="clear">
+            <span class="threestars"></span>
+            <div class="persons">2-4 persons</div>
+            <div class="centerdistance">500m</div>
+            <div class="seadistance">1000m</div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="width33">
-			<div class="galleryaccommodation">
-      </div>
-      <div class="accommodationinfo">
-        <div class="accommodationheadline">Apartment Skalice</div>
-        <hr>
-        <span class="threestars"></span>
-        <div class="persons">2-4 persons</div>
-        <div class="centerdistance">500m</div>
-        <div class="seadistance">1000m</div>
-      </div>
-    </div>
-		<div class="width33">
-      <div class="galleryaccommodation">
-      </div>
-      <div class="accommodationinfo">
-        <div class="accommodationheadline">Apartment Skalice</div>
-        <hr>
-        <span class="threestars"></span>
-        <div class="persons">2-4 persons</div>
-        <div class="centerdistance">500m</div>
-        <div class="seadistance">1000m</div>
-      </div>
-    </div>
-		<div class="width33">
-      <div class="galleryaccommodation">
-      </div>
-      <div class="accommodationinfo">
-        <div class="accommodationheadline">Apartment Skalice</div>
-        <hr>
-        <span class="threestars"></span>
-        <div class="persons">2-4 persons</div>
-        <div class="centerdistance">500m</div>
-        <div class="seadistance">1000m</div>
-      </div>
-    </div>
-    <div class="sliderCars">
-      CAROUSEL
     </div>
   </div>
-</div>
 </div>`)
