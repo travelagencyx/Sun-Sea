66676c16d24400f9834af54b40797bb0cd3bd03a
travelagencyx <travelagencyx@gmail.com>
2016-02-15 15:47:21 +0100

    Added hover stop for carousel

--- js/carousel.js
+++ js/carousel.js
@@ -3,40 +3,49 @@ var carouselModule = (function (){
 	var _carouselSelector;
 	var _imgArr = [];
 	var _count = 0;
+	var fadetimeout;
+	var slidetimeout;
+	
 	
 	//Fade out function
 	function fadeout(){
-	document.querySelector('.' + _carouselSelector).style.opacity = 0;
+		_carouselSelector.style.opacity = 0;
 	}
 
 	var position = 0; //position is set to zero
 
 	function slidingImages() {
-	if ( document.images ) {
-		if ( position < _count ) {
-			imageSrc(position); // call the image source
-			position += 1;
-		} else {
-			imageSrc(0);
-			position = 1;
+		if ( document.images ) {
+			if ( position < _count ) {
+				imageSrc(position); // call the image source
+				position += 1;
+			} else {
+				imageSrc(0);
+				position = 1;
+			}
 		}
-	}
-	setTimeout(fadeout, 4500);
-	setTimeout(slidingImages, 5000);
+		fadetimeout = setTimeout(fadeout, 5500);
+		slidetimeout = setTimeout(slidingImages, 6000);
 	}
 
 	function imageSrc(position) {
-	document.querySelector('.' + _carouselSelector).style.backgroundImage  = _imgArr[position];
-	document.querySelector('.' + _carouselSelector).style.opacity = 1;
+		_carouselSelector.style.backgroundImage  = _imgArr[position];
+		_carouselSelector.style.opacity = 1;
 	}
 	
+	
+
 	return{
 	//First argument - array of images; Second argument - carousel class example: 'accommodationcarousel'
 		init: function(Args){
 		_args=Args;
 		_imgArr = _args[0];
-		_carouselSelector = _args[1];
+		_carouselSelector = document.querySelector('.' + _args[1]);
 		_count = _imgArr.length; // assign images array length
+		_carouselSelector.addEventListener("mouseover", function(){ clearTimeout(fadetimeout);});
+		_carouselSelector.addEventListener("mouseover", function(){ clearTimeout(slidetimeout);});	
+		_carouselSelector.addEventListener("mouseout", function(){ fadetimeout = setTimeout(fadeout, 5500);});
+		_carouselSelector.addEventListener("mouseout", function(){ slidetimeout = setTimeout(slidingImages, 6000);});
 		},
 		slidingImages: slidingImages
 	};
